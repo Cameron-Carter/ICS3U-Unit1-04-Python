a97e11d66a8ff6a0a2338271a086951248218645
Cameron Carter <cameron.carter@mths.ca>
2021-04-23 15:25:47 +0000

    made area and perimeter calculator

---
+++ area_and_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Cameron Carter
+# Created on April 2021
+# This program calculates the perimeter and area of a 5cm by 3cm rectangle
+
+
+def main():
+    print("If a rectangle has the following dimensions:")
+    print("5cm by 3cm")
+    print("")
+    print("The area is {}cm^2.".format(5 * 3))
+    print("The perimeter is {}cm.".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
