33ca3d0feda1952f356849a3eb570afc9ed06fc9
Cameron Carter <cameron.carter@mths.ca>
2021-04-26 14:18:39 +0000

    final changes

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -6,11 +6,15 @@
 
 
 def main():
+    # this function calculates perimeter and area
+    
     print("If a rectangle has the following dimensions:")
-    print("5cm by 3cm")
+    print("5 cm by 3 cm")
     print("")
-    print("The area is {}cm^2.".format(5 * 3))
-    print("The perimeter is {}cm.".format(2 * (5 + 3)))
+    print("The area is {} cm².".format(5 * 3))
+    print("The perimeter is {} cm.".format(2 * (5 + 3)))
+    print("")
+    print("Done.")
 
 
 if __name__ == "__main__":
